c84123325b94806748c421190e3b4ec2c3b63215
Woody Chang <kazettique@gmail.com>
2023-10-03 07:57:37 +0800

    update

--- src/components/TextEditor.vue
+++ src/components/TextEditor.vue
@@ -177,6 +177,10 @@ onMounted(() => {
 </script>
 
 <style>
+.button {
+  @apply bg-slate-700 hover:bg-slate-600 text-gray-200 rounded-full px-4 py-2 uppercase disabled:bg-slate-500 disabled:cursor-not-allowed;
+}
+
 .wysiwyg-output h1 {
   @apply text-2xl;
   @apply font-bold;
